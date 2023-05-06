253cc55d16d85532ea5e9ae4a35e841d5a8b9828
Nikhil Saraswat <79843717+nikhilsaraswat2605@users.noreply.github.com>
2023-05-06 17:32:15 +0530

    uploaded

---
+++ Multimedia-Management-System/Multimedia-Management-System-Backend/utils/config.js
@@ -0,0 +1,13 @@
+const config = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "Nikhil2002#",
+    database: "multimediadb",
+  }
+};
+
+const jwtSecret = "secret";
+
+module.exports.config = config;
+module.exports.jwtSecret = jwtSecret;
